42c8dd4c3e738d508ca57cfa513b4e15d5907062
marriol <mcarriol@asu.edu>
2026-03-15 12:29:08 -0500

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import S31Rx from "./pages/S31Rx";
+import Peptides from "./pages/Peptides";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path={"/"} component={S31Rx} />
+      <Route path={"/peptides"} component={Peptides} />
+      <Route path={"/404"} component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
